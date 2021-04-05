00d3e6e27f7b8e5e19ff0349fc9892ec5d76a2ea
Ben Hutton <relequestual+git@gmail.com>
2021-04-05 21:14:06 +0100

    Add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,12 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', type: 'image/png', sizes:"32x32", href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes:"32x32", href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes:"16x16", href: '/favicon-16x16.png' }
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
