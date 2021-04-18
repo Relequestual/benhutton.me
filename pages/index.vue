68f3054b286fd02686f53738611aeb76177cc254
Ben Hutton <relequestual+git@gmail.com>
2021-04-19 00:08:28 +0100

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,23 @@
   <div
     class="container bg-black bg-center bg-contain bg-no-repeat mx-auto grid min-h-screen justify-center"
   >
-    <div class="border border-white my-auto">
-      <div class="">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <!-- <a
+    <div class="my-auto">
+      <div class="border-2 border-white my-auto bg-gray-50 bg-opacity-50">
+        <img
+          class="mx-auto rounded-full -mt-28 mb-4 border-2 border-white"
+          src="~/assets/face.jpg?resize&size=150"
+        />
+        <h1 class="text-4xl text-center mb-4">Ben Hutton</h1>
+        <div class="">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="button--green"
+          >
+            Documentation
+          </a>
+          <!-- <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           rel="noopener noreferrer"
@@ -20,19 +26,20 @@
         >
           GitHub
         </a> -->
+        </div>
       </div>
-    </div>
-    <div>
-      Photo by
-      <a
-        href="https://unsplash.com/@grakozy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-        >Greg Rakozy</a
-      >
-      on
-      <a
-        href="https://unsplash.com/collections/9415895/for-json-schema/717d1c58e33fd8f0119ed72f768a584a?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-        >Unsplash</a
-      >
+      <div>
+        Photo by
+        <a
+          href="https://unsplash.com/@grakozy?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          >Greg Rakozy</a
+        >
+        on
+        <a
+          href="https://unsplash.com/collections/9415895/for-json-schema/717d1c58e33fd8f0119ed72f768a584a?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+          >Unsplash</a
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -57,14 +64,4 @@ export default {
 .container {
   background-image: url('~/assets/greg-rakozy-oMpAz-DN-9I-unsplash.jpg?resize&size=1920');
 }
-
-/* .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-} */
 </style>
